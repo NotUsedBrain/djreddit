51a6ec8a3b3afed5ea8eb6996721d3af6e783da9
NotUsedBrain <notusedbrain@hotmail.com>
2019-08-29 18:26:09 +0100

    fixed main page so localhost:3000 shows all posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
     resources :comments
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "posts#index"  #defining the root url
 end
